e963da3bf3d0818411a9e4f66bb2351b01eeba54
Kitiya Mettarat <Kitiya.Mettarat@hotmail.com>
2025-01-02 19:25:11 +0700

    add validation

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -6,7 +6,17 @@ namespace Catalog.API.Products.UpdateProduct
         : ICommand<UpdateProductResult>;
 
     public record UpdateProductResult(bool IsSuccess);
-        
+
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(command => command.Id).NotEmpty().WithMessage("Product ID is required");
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Name is required")
+                .Length(2, 150).WithMessage("Name must be between 2 and 150 characters");
+        }
+    }
 
     internal class UpdateProductCommandHandler
         (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
