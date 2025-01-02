33b8f5b00b732739721d09b7b0523f940a05aa34
Kitiya Mettarat <Kitiya.Mettarat@hotmail.com>
2025-01-02 15:44:16 +0700

    add catalog api

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,40 @@
+﻿using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.UpdateProduct
+{
+    public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<UpdateProductResult>;
+
+    public record UpdateProductResult(bool IsSuccess);
+        
+
+    internal class UpdateProductCommandHandler
+        (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
+        : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+            //Business logic to create a product.
+
+            //1. create Product entity from command object
+            var product = await session.LoadAsync<Product>(command.Id, cancellationToken);
+            if (product is null)
+            {
+                throw new ProductNotFoundException();
+            }
+
+            product.Name = command.Name;
+            product.Category = command.Category;
+            product.Description = command.Description;
+            product.ImageFile = command.ImageFile;
+            product.Price = command.Price;            
+
+            //2. save to database
+            session.Update(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            //3. return CreateProductResult result
+            return new UpdateProductResult(true);
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.Extensions.Options;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+var test = builder.Configuration.GetConnectionString("Database")!;
+
+builder.Services.AddMarten(options =>
+{
+    //"host=localhost;database=store;password=postgres;username=postgres"
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);       
+}).UseLightweightSessions();
+
+var app = builder.Build();
+
+//Configure the HTTP request pipeline.
+app.MapCarter();
+
+app.Run();
